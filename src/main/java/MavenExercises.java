b83eb898e376ee683866876661f52fe77ccc6ef6
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-01-08 10:57:38 -0600

    refactor: MavenExercises class to replace sout messages with conditional logic for the exerises

--- src/main/java/MavenExercises.java
+++ src/main/java/MavenExercises.java
@@ -1,37 +1,35 @@
-import jdk.swing.interop.SwingInterOpUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.Scanner;
 
 public class MavenExercises {
-    //Creating Scanner
-    private Scanner scanner;
-
-    public MavenExercises() {
-        this.scanner = new Scanner(System.in);
-    }
-
-    public String getString() {
-        return this.scanner.nextLine();
-    }
 
     //Main Method
     public static void main(String[] args) {
-        //Creating a new instance of Scanner
-        MavenExercises userInput = new MavenExercises();
+        System.out.print("Enter Something:");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
 
-//        //TODO: Tells whether or not what the user entered is a number
-//        System.out.println("Enter Something"); //Ha13rry OR 43dfd543 (both equate to false)
-//        System.out.println(StringUtils.isNumeric(userInput.getString())); //false
+        //TODO: Tells whether or not what the user entered is a number
+        if(StringUtils.isNumeric(userInput)) {
+            System.out.printf("%s is a number %n", userInput);
+        } else {
+            System.out.printf("You Entered: \"%s\" %n", userInput);
+            //TODO: Tells whether or not what the user entered is a number
+            System.out.printf("\"%s\" is not a number %n", userInput);
 
+            //TODO: Flips the case of the string
+            System.out.printf("Flipped Case: %s %n", StringUtils.swapCase(userInput));
 
-//        //TODO: Flips the case of the string
-//        System.out.println("Enter a String"); //Harry Potter
-//        System.out.println(StringUtils.swapCase(userInput.getString())); //hARRY pOTTER
+            //TODO: Reverses the string
+            System.out.printf("Reversed: %s %n", StringUtils.reverse(userInput));
+        }
 
-
-        //TODO: Reverses the string
-        System.out.println("Enter a String"); //Harry
-        System.out.println(StringUtils.reverse(userInput.getString())); //yrraH
+        //OUTPUT:
+                //Enter Something: Life, The Universe, and Everything
+                //You Entered: "Life, The Universe, and Everything"
+                //"Life, The Universe, and Everything" is not a number
+                //Flipped Case: lIFE, tHE uNIVERSE, AND eVERYTHING
+                //Reversed: gnihtyrevE dna ,esrevinU eht ,efiL
     }
 }
